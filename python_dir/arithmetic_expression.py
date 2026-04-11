02ef06260b69d9f53764eba3a69296cd106d01a8
MarkSlav <msantoferreira@gmail.com>
2026-04-11 19:08:18 -0400

    Studies in arithmetic expression in python

---
+++ python_dir/arithmetic_expression.py
@@ -0,0 +1,22 @@
+x = 13
+y = 3
+print(" x plus y evaluates to ...")
+print(x + y)
+
+print("x minus y evaluates to ...")
+print(x - y)
+
+print("x times y evaluates to ...")
+print(x * y)
+
+print("x to the power of y evaluates to...")
+print(x ** y)
+
+print("x divided by y evaluates to...")
+print(x / y)
+
+print("x floor divided by y evaluates to...")
+print(x // y)
+
+print("x modulo y evaluates to...")
+print(x % y)
