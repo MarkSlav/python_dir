b5e26b6e9fd7dd159d6ac736b5eba0b0fcc4a315
MarkSlav <msantoferreira@gmail.com>
2026-04-11 18:09:46 -0400

    Studies about input and arithmetic

---
+++ python_dir/input.py
@@ -0,0 +1,27 @@
+x = int(input("Enter a Value:  "))
+y = int(input("Enter a multiplier: "))
+r = x * y
+
+print(f"Result is: {r}!")
+
+print("================")
+print("Division")
+x = float(input("Enter a value: "))
+y = float(input("Enter a Division: "))
+r = x / y
+print(f"Result is: {r}!")
+
+print("================")
+print("Subtraction")
+x = int(input("Enter a Value: "))
+y = int(input("Enter a subtraction: "))
+r = x - y
+print(f"Result is: {r}!")
+
+print("================")
+print("Addition")
+x = int(input("Enter a Value: "))
+y = int(input("Enter a Add: "))
+r = x + y
+print(f"Result is: {r}!")
+
