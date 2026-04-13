8f0158737b84fde178c4c3a4cc897a162cfc9d33
MarkSlav <msantoferreira@gmail.com>
2026-04-13 13:03:08 -0400

    built-in functions

---
+++ python_dir/input_output.py
@@ -0,0 +1,74 @@
+#print
+print("My name's Marcos")
+print(93)
+print(900.89)
+
+#input
+input ("Enter your Name: ")
+input ("Submit (yes/no)? ")
+input(55)
+
+#TYPE CASTING
+#str(x)
+str(32)
+str(12.95)
+str(-4)
+
+#int (x)
+int("101")
+int(2.78)
+int("99.99")
+
+#float(x)
+float(18.214)
+float(9)
+float("potato")
+
+#INTEGERS AND FLOATS
+#abs(x)
+#eturns the absolute value of x.
+abs(15)
+abs(-8)
+abs(-94.32)
+
+#round(x)
+#Returns the number x rounded to the nearest integer. If the decimal is exactly 
+#\[0.5\], it rounds up or down to whichever integer is even.
+round(3.141)
+round(3.72)
+round(3.5)
+
+#round(x, y)
+#Returns the number x rounded to the yth decimal place.
+round(6.4368,1)
+round(6.4368,2)
+round(6.4368,3)
+
+#min(x, y)
+#Returns the lesser of the x and y values. Can also find the minimum of more than two input values.
+min(4,7)
+min(10,-10)
+min(3,8,2,6)
+
+#max(x, y)
+#Returns the greater of the x and y values. Can also find the maximum of more than two input values.
+
+#STRINGS
+
+#len(x)
+#Returns the number of characters in the string x. The length of a string includes all characters: letters, numbers, symbols, and spaces.
+len("exit")
+len("Save as...")
+len("")
+
+#min(x, y)
+#Returns the lesser of the x and y values. The lesser string comes first in a case-sensitive dictionary order: 0-9, A-Z, a-z.
+min("aardvark", "max")
+min("python", "3")
+min("adam","Adam")
+
+#max(x, y)
+#Returns the greater of the x and y values. The greater string comes last in a case-sensitive dictionary order: 0-9, A-Z, a-z.
+max("aardvark, "max")
+max("chalie", "Zia")
+max("word1","word2","word3")
